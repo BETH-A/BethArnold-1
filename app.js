ca3ef0d0f9f42502dba694802e7e83a1a54c509f
Beth-A <elizabeth78@me.com>
2020-04-27 14:20:52 -0400

    Working on app.js

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res){
-  res.send("");
+  res.sendFile(__dirname + '/portfolio.html');
 });
 
 app.get("/contact",function(req, res){
-  res.send("");
+  res.sendFile(__dirname + '/contact.html');
 });
 
 app.get("/about", function(req, res){
-  res.send("");
+  res.sendFile(__dirname + '/index.html');
 });
 
-
-app.get("/hobbies", function(req, res){
-  res.send("");
-});
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
