67ba6b28628b6c648038e16e60a56838d15e2274
Beth-A <elizabeth78@me.com>
2020-12-11 18:49:52 -0500

    committed out express

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 //import express from './node_modules/express/dist/express.js'; // dist/express.js
 
-const app = express();
+// const app = express();
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + '/');
-});
+// app.get("/", function(req, res){
+//   res.sendFile(__dirname + '/');
+// });
 
-app.get("/contact",function(req, res){
-  res.sendFile(__dirname + '/contact.html');
-});
+// app.get("/contact",function(req, res){
+//   res.sendFile(__dirname + '/contact.html');
+// });
 
-app.get("/about", function(req, res){
-  res.sendFile(__dirname + '/about.html');
-});
+// app.get("/about", function(req, res){
+//   res.sendFile(__dirname + '/about.html');
+// });
 
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000");
-});
+// app.listen(3000, function(){
+//   console.log("Server started on port 3000");
+// });
